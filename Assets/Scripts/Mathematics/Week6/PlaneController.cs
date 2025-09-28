2c2a98f82e70dbb96bf2d917668d29f6001b9429
Sarluni <giovanna.arroyo@tecsup.edu.pe>
2025-09-27 22:55:56 -0500

    movement plane

--- Assets/Scripts/Mathematics/Week6/PlaneController.cs
+++ Assets/Scripts/Mathematics/Week6/PlaneController.cs
@@ -13,30 +13,34 @@ namespace Mathematics.Week6
         [SerializeField] private float rollPlane;
         [SerializeField] private float rollhGain = 1f;
         [SerializeField] private MinMax rollTreshHold;
+        [SerializeField] private float yawPlane;
+        [SerializeField] private float yawGain = 1f;
+        [SerializeField] private MinMax yawTreshHold;
 
         [Header("Rotation Data")]
-        [SerializeField] private Quaternion qx = Quaternion.identity; //<0,,0,0,1>
-        [SerializeField] private Quaternion qy = Quaternion.identity; //<0,,0,0,1>
-        [SerializeField] private Quaternion qz = Quaternion.identity; //<0,,0,0,1>
+        [SerializeField] private Quaternion qx = Quaternion.identity;
+        [SerializeField] private Quaternion qy = Quaternion.identity;
+        [SerializeField] private Quaternion qz = Quaternion.identity;
+        [SerializeField] private Quaternion r = Quaternion.identity;
 
-        [SerializeField] private Quaternion r = Quaternion.identity; //<0,,0,0,1>
         private float anguloSen;
         private float anguloCos;
 
         protected float _pitchDirection = 0f;
         protected float _rollDirection = 0f;
+        protected float _yawDirection = 0f;
 
         private void FixedUpdate()
         {
             pitchPlane += _pitchDirection * pitchGain;
-
             pitchPlane = Mathf.Clamp(pitchPlane, pitchTreshHold.MinValue, pitchTreshHold.MaxValue);
 
             rollPlane += _rollDirection * rollhGain;
-
             rollPlane = Mathf.Clamp(rollPlane, rollTreshHold.MinValue, rollTreshHold.MaxValue);
 
-            //rotacion z -> x -> y
+            yawPlane += _yawDirection * yawGain;
+            yawPlane = Mathf.Clamp(yawPlane, yawTreshHold.MinValue, yawTreshHold.MaxValue);
+
             anguloSen = Mathf.Sin(Mathf.Deg2Rad * rollPlane * 0.5f);
             anguloCos = Mathf.Cos(Mathf.Deg2Rad * rollPlane * 0.5f);
             qz.Set(0, 0, anguloSen, anguloCos);
@@ -45,58 +49,30 @@ namespace Mathematics.Week6
             anguloCos = Mathf.Cos(Mathf.Deg2Rad * pitchPlane * 0.5f);
             qx.Set(anguloSen, 0, 0, anguloCos);
 
-            /*anguloSen = Mathf.Sin(Mathf.Deg2Rad * rollPlane * 0.5f);
-            anguloCos = Mathf.Cos(Mathf.Deg2Rad * rollPlane * 0.5f);
-            qy.Set(0, anguloSen, 0, anguloCos);*/
+            anguloSen = Mathf.Sin(Mathf.Deg2Rad * yawPlane * 0.5f);
+            anguloCos = Mathf.Cos(Mathf.Deg2Rad * yawPlane * 0.5f);
+            qy.Set(0, anguloSen, 0, anguloCos);
 
-            //multiplicación y -> x -> z
             r = qy * qx * qz;
-
             transform.rotation = r;
 
             UpdatePosition();
         }
 
-        
-
-        //Pitch -> X Axis
         public void RotatePitch(InputAction.CallbackContext context)
         {
             _pitchDirection = context.ReadValue<float>();
         }
 
-        //Roll -> Z Axis
         public void RotateRoll(InputAction.CallbackContext context)
         {
             _rollDirection = context.ReadValue<float>();
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        public void RotateYaw(InputAction.CallbackContext context)
+        {
+            _yawDirection = context.ReadValue<float>();
+        }
 
         private float _verticalDirection = 0f;
         private float _horizontalDirection = 0f;
@@ -121,7 +97,10 @@ namespace Mathematics.Week6
 
         private void UpdatePosition()
         {
-            _myRB.linearVelocity = new Vector3(-_horizontalDirection * velocitySpeed, -_verticalDirection * velocitySpeed, 0f);
+            Vector3 move = transform.forward * velocitySpeed;
+            move += transform.right * _horizontalDirection * velocitySpeed;
+            move += transform.up * _verticalDirection * velocitySpeed;
+            _myRB.velocity = move;
         }
     }
 
